10f9af038314c1d117b5609ac0cafce87efac94f
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-08 02:16:43 -0500

    Added heading, seach bar, and mock table for view setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [giphySearch, setGiphySearch] =useState('Paris');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Giphy Search</h1>
+      <div>
+        <label>
+          Search Giphy: 
+          <input 
+            type="text" 
+            value={giphySearch}
+            onChange={e => setGiphySearch(e.target.value)}
+          />
+        </label>
+        <button>Search</button>
+      </div>
+
+      <div>
+        <h2>Results</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>Test1</td></tr>
+            <tr></tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
